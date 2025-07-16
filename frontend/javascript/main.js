37767a6c93ae22d23235d0e95b670b03a0d8d07b
CloudWithVithu <vithushanvisuvalingam@gmail.com>
2025-07-16 09:12:51 +0530

    azure function url get connected with main function

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
   try {
-    const res = await fetch('');
+    const res = await fetch('https://resumchallengfunapp-g2a7e9dfh8b4eceq.centralus-01.azurewebsites.net/api/GetResumeCounter?code=idngy4K9PEdSOSK228W76xtZdmAANVCM0XHGqDuGDgzMAzFuw-gCaA==');
     const data = await res.json();
     document.getElementById('visitCount').innerText = data.count;
   } catch (err) {
